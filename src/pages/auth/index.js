b2286884b6865d0a896eed7453c66aed256b8f03
wifi_mug <yaxding@gmail.com>
2024-01-06 00:50:46 +0000

    initial base for campsync

---
+++ src/pages/auth/index.js
@@ -0,0 +1,37 @@
+import { auth, provider } from "../../config/firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate, Navigate } from "react-router-dom";
+import { useGetUserInfo } from "../../hooks/useGetUserInfo";
+import './styles.css';
+
+export const Auth = () => {
+
+    const { isAuth } = useGetUserInfo();
+    const navigate = useNavigate();
+    
+
+    const signInWithGoogle = async () => {
+        const results = await signInWithPopup(auth, provider);
+        const authInfo = {
+            userID: results.user.uid,
+            name: results.user.displayName,
+            profilePhoto: results.user.photoURL,
+            isAuth: true,
+        };
+        localStorage.setItem("auth", JSON.stringify(authInfo));
+        navigate("/expense-tracker");
+    }
+
+
+    if (isAuth) {
+        return <Navigate to="/expense-tracker" />
+    }
+    
+    return <div className="login-page">
+        <p>Sign in with Google to Continue</p>
+        <button className="sign-in-with-google-btn" onClick={signInWithGoogle}> 
+        {""}
+            Sign in with Google 
+        </button>
+    </div>;
+}
---
+++ src/pages/expense-tracker/index.js
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import { useAddTransaction } from "../../hooks/useAddTransaction";
+import { useGetTransactions } from "../../hooks/useGetTransactions";
+import { useGetUserInfo } from "../../hooks/useGetUserInfo";
+import './styles.css';
+import { signOut } from "firebase/auth";
+import { auth } from "../../config/firebase-config";
+import { useNavigate } from "react-router-dom";
+
+export const ExpenseTracker = () => {
+    const { addTransaction } = useAddTransaction();
+    const { name, profilePhoto } = useGetUserInfo();
+    const { transactions, transactionTotals } = useGetTransactions();
+    const { balance, income, expenses } = transactionTotals;
+    const navigate = useNavigate();
+    
+    const [description, setDescription] = useState("");
+    const [transactionAmount, setTransactionAmount] = useState(0);
+    const [transactionType, setTransactionType] = useState("expense");
+
+    const signUserOut = async () => {
+        try {
+            await signOut(auth);
+            localStorage.clear();
+            navigate("/");
+        } catch (err) {
+            console.err(err);
+        }
+
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        addTransaction({
+            description, 
+            transactionAmount, 
+            transactionType});
+        setDescription("");
+        setTransactionAmount(0);
+    };    
+    return (
+    <>
+    <div className="expense-tracker">
+        <div className="container">
+            <h1> {name}'s Expense Tracker </h1>
+            <div className="balance">
+                <h3> Your Balance </h3>
+                {balance >= 0 ? (
+                    <h2> ${balance} </h2>
+                ) : (
+                    <h2> -${balance * -1} </h2>
+                )}
+                
+            </div>
+            <div className="summary">
+                <div className="income"></div>
+                <h4> Income </h4>
+                <p> ${income} </p>
+                <div className="expenses"></div>
+                <h4> Expenses </h4>
+                <p> ${expenses} </p>
+            </div>
+            <form className="add-transaction" onSubmit={onSubmit}>
+                <input type="text" 
+                placeholder="Description" 
+                value={description}
+                required 
+                onChange={(e) => setDescription(e.target.value)}/>
+                <input type="number" 
+                placeholder="Amount" 
+                value={transactionAmount}
+                required 
+                onChange={(e) => setTransactionAmount(e.target.value)}/>
+                <input type="radio" 
+                id="expense"
+                 value="expense" 
+                 checked={transactionType === "expense"}
+                 required 
+                 onChange={(e) => setTransactionType(e.target.value)} />
+                <label htmlFor="expense"> Expense </label>
+                <input type="radio" 
+                id="income" 
+                value="income" 
+                checked={transactionType === "income"}
+                required 
+                onChange={(e) => setTransactionType(e.target.value)} />
+                <label htmlFor="income"> Income </label>
+                <button type="submit"> Add Transaction </button>
+            </form>
+        </div>
+        {profilePhoto && (
+        <div className="profile"> 
+            { " " }
+            <img className="profile-photo" src={profilePhoto} width="200" />
+            <button className="sign-out-btn" onClick={signUserOut}> 
+                Sign Out
+            </button>
+
+        </div>
+        )}
+        
+    </div>
+    <div className="transactions">
+        <h3> Transactions </h3>
+        <ul>
+            {transactions.map((transaction) => {
+                const {description, transactionAmount, transactionType} = transaction;
+                return (
+                    <li>
+                        <h4> { description } </h4>
+                        <p>
+                            ${ transactionAmount } . <label style={{color: transactionType === "expense" ? "red" : "green"}}> { transactionType } </label>
+                        </p>
+                    </li>
+                )
+            })}
+        </ul>
+
+    </div>
+    </>);
+}
