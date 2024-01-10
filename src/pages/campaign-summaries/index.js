f1bccc3bc900a6ae7e07326bd2fecb621320cd04
charrink <68334961+charrink@users.noreply.github.com>
2024-01-10 17:32:00 +0000

    Campaign summaries shows correct information after redirect (#2)
    * useGetCampaignByID implemented
    
    useGetCampaignByID collects the correct information from local storage, and is used to pass that campaign's data to campaign summaries
    
    * useGetCampaignByID passes correct information
    
    useGetCampaignByID now correctly passes the data and not the reference
    
    * can retrieve campaign summary from firestore
    
    * removed hardcoded variable
    
    * fixed bug where localstorage was not being updated in time
    
    ---------
    
    Co-authored-by: wifi_mug <yaxding@gmail.com>

--- src/pages/campaign-summaries/index.js
+++ src/pages/campaign-summaries/index.js
@@ -1,3 +1,17 @@
+import { useGetCampaignByID } from "../../hooks/useGetCampaignByID"
+
+
 export const CampaignSummaries = () => {
-    return <p>CampaignSummaries</p>
+    const campaignID = localStorage.getItem("currentCampaign");
+    //console.log(campaignID);
+    const {campaignName, campaignDesc} = useGetCampaignByID(campaignID);
+
+    //console.log("outside hook");
+
+    return <>
+        <h1>Campaign Summaries</h1>
+        <p>Campaign ID: {campaignID}</p>
+        <p>Campaign name: {campaignName}</p>
+        <p>Campaign description: {campaignDesc}</p>
+    </>
 }
