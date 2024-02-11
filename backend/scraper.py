d947088f9a8f1bcb201648e9ddd31eff56c2d6c4
wifi_mug <yaxding@gmail.com>
2024-02-11 15:38:33 +0000

    add post data for getting character data for different user ids

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,14 +1,19 @@
 import time
-from flask import Flask
+from flask import (
+    Flask,
+    request
+    )
 
 import requests
 
 app = Flask(__name__)
 
 
-@app.route('/character-data')
+@app.route('/character-data', methods=("GET", "POST"))
 def get_character_data():
-    page = requests.get("https://character-service.dndbeyond.com/character/v5/character/101991519/")
-    print(page.status_code)
-    print(page.text)
-    return page.text
+    if request.method == 'POST':
+        characterid = request.json['characterid']
+        page = requests.get("https://character-service.dndbeyond.com/character/v5/character/" + characterid + "/")
+        # print(page.status_code)
+        # print(page.text)
+        return page.text
