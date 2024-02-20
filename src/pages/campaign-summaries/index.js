acbee633004ee8d75d865deae6690f87b825e8cf
wifi_mug <yaxding@gmail.com>
2024-02-20 15:21:37 +0000

    can retrieve and display corresponding summaries to campaigns

--- src/pages/campaign-summaries/index.js
+++ src/pages/campaign-summaries/index.js
@@ -1,6 +1,7 @@
 import { useGetCampaignByID } from "../../hooks/useGetCampaignByID"
 import { NavBar } from "../../navbar";
 import { useNavigate } from 'react-router-dom';
+import { useGetSummary } from "../../hooks/useGetSummary";
 
 
 
@@ -8,6 +9,7 @@ export const CampaignSummaries = () => {
     const campaignID = localStorage.getItem("currentCampaign");
     //console.log(campaignID);
     const navigate = useNavigate();
+    const summaries = useGetSummary(campaignID);
     const {campaignName, campaignDesc} = useGetCampaignByID(campaignID);
 
     const onSubmit = (e) => {
@@ -15,7 +17,7 @@ export const CampaignSummaries = () => {
       navigate("/add-summary", {state: {campaignID: campaignID}})
     }
     console.log("summaries page");
-
+    console.log(summaries.length)
     return (
       <main>
         <NavBar/>
@@ -23,6 +25,15 @@ export const CampaignSummaries = () => {
         <p>Campaign ID: {campaignID}</p>
         <p>Campaign name: {campaignName}</p>
         <p>Campaign description: {campaignDesc}</p>
+        <p>Summaries:     
+          {summaries.map(item => {
+            return (
+              <ul>
+                <li>{item}</li>
+              </ul>
+            )
+          })}
+        </p>
 
         <button onClick={onSubmit}>
             Add Summary
